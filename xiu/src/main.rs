11b6fdecf4c5be71f52bf12c70d736db67948efb
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2022-06-17 23:48:29 +0200

    Improved shutdown signal handler

--- xiu/src/main.rs
+++ xiu/src/main.rs
@@ -47,8 +47,37 @@ async fn main() -> Result<()> {
   let mut service = Xiu::new(config);
   service.run().await?;
 
-  signal::ctrl_c().await?;
+  shutdown_signal().await;
+
   Ok(())
+}
+
+async fn shutdown_signal() {
+  let ctrl_c = async {
+    signal::ctrl_c()
+      .await
+      .expect("failed to install Ctrl+C handler");
+  };
+
+  #[cfg(unix)]
+  {
+    let terminate = async {
+      signal::unix::signal(signal::unix::SignalKind::terminate())
+        .expect("failed to install signal handler")
+        .recv()
+        .await;
+    };
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+  }
+
+  #[cfg(not(unix))]
+  ctrl_c.await;
+
+  log::info!("signal received, starting graceful shutdown");
 }
 
 pub struct Xiu {
